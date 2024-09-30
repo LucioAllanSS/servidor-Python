1172af210107df8b044bcbce44430cbffda22ae9
Lucio Allan <lucioallanss@gmail.com>
2024-09-30 20:15:42 -0300

    probando 3b

--- server.py
+++ server.py
@@ -3,8 +3,8 @@ import json
 from llama_cpp import Llama
 
 llm = Llama.from_pretrained(
-    repo_id="bartowski/Llama-3.2-1B-Instruct-GGUF",
-    filename="Llama-3.2-1B-Instruct-Q8_0.gguf",
+    repo_id="bartowski/Llama-3.2-3B-Instruct-GGUF",
+    filename="Llama-3.2-3B-Instruct-IQ3_M.gguf",
 )
 
 # Crear la respuesta del chat
